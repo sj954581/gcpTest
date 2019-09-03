c8bf96bb061c0cb20ae390de36ca29aae01c2d1c
unknown <sj954581@gmail.com>
2019-09-03 12:59:39 +0530

    GCP first Commit

---
+++ src/main/java/com/gcpTestApp/GcpTestAppApplication.java
@@ -0,0 +1,13 @@
+package com.gcpTestApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpTestAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/gcpTestApp/controller/GcpTestAppController.java
@@ -0,0 +1,30 @@
+package com.gcpTestApp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+
+@RestController
+public class GcpTestAppController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String getText1() {
+		String str = "Hey!!! Default Api from GCP Srevice... ";
+		return str;
+	}
+	
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public String getText2() {
+		String str = "Hey!!! test Api from GCP Srevice... ";
+		return str;
+	}
+}
